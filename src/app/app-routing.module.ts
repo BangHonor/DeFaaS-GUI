8e24c29595fb90502e4f1bd6c0ad532d329a95bf
kitchen <kitchen_work@163.com>
2021-04-20 01:35:56 +0800

    add console

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'account', loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule) },
-  { path: 'faaslevel', loadChildren: () => import('./pages/faastoken/faastoken.module').then(m => m.FaastokenModule) }
+  { path: 'faaslevel', loadChildren: () => import('./pages/faastoken/faastoken.module').then(m => m.FaastokenModule) },
+  { path: 'console', loadChildren: () => import('./pages/console/console.module').then(m => m.ConsoleModule) },
 ];
 
 @NgModule({
